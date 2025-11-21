96e6ece6b49e0fdbdae94b660ada49f4eeaaa7bc
Mason Ticehurst <masonticehurst@me.com>
2025-11-20 23:05:04 -0500

    Add arrow / turn signal bitmap image, configuring message filers

--- src/bxcan.adb
+++ src/bxcan.adb
@@ -1,4 +1,4 @@
-with LED_Blinker;
+with Cortex_M.Cache;
 with Exception_Handler;
 with SDCard;
 
@@ -27,7 +27,7 @@ procedure bxcan is
    Loop_Iter           : Natural             := 0;
    Frame               : STM32.CAN.CAN_Frame (STM32.CAN.Standard);
    CAN_Status          : STM32.CAN.CAN_Status;
-   Period              : constant Time_Span  := Milliseconds (555);
+   Period              : constant Time_Span  := Milliseconds (222);
    Next_Release        : Time                := Clock;
    Last_Steering_Angle : Long_Float := CAN_Handler.Steering_Angle_Degrees;
 begin
@@ -40,6 +40,27 @@ begin
 
    -- CAN Bus Initialization
    STM32.CAN.Initialize (STM32.CAN.CAN_1, STM32.CAN.CAN_500K);
+
+   declare
+      Filter0 : constant CAN_Filter_Config :=
+        (Bank => 0, FIFO => 0, Mode => List, ID_Type => STM32.CAN.Standard,
+         Id1        => 16#118#, Id2 => 16#129#, Mask1 => 0, Mask2 => 0,
+         Use_32_Bit => True);
+
+      Filter1 : constant CAN_Filter_Config :=
+        (Bank => 1, FIFO => 0, Mode => List, ID_Type => STM32.CAN.Standard,
+         Id1        => 16#257#, Id2 => 16#3F5#, Mask1 => 0, Mask2 => 0,
+         Use_32_Bit => True);
+
+      Filter2 : constant CAN_Filter_Config :=
+        (Bank => 2, FIFO => 0, Mode => List, ID_Type => STM32.CAN.Standard,
+         Id1        => 16#405#, Id2 => 16#000#, Mask1 => 0, Mask2 => 0,
+         Use_32_Bit => True);
+   begin
+      Configure_Filter (CAN_1, Filter0);
+      Configure_Filter (CAN_1, Filter1);
+      Configure_Filter (CAN_1, Filter2);
+   end;
 
    -- SD Card initialization
    STM32.Board.SDCard_Device.Initialize;
@@ -261,18 +282,18 @@ begin
    --  Frame.Data (8)    := 16#08#;
    --  Frame.RTR         := False;
 
+   -- Top Row
+   Draw_Info (Point => (X => 9, Y => 122), Text => "TBD");
+   Draw_Info (Point => (X => 128, Y => 122), Text => "TBD");
+   Draw_Info (Point => (X => 248, Y => 122), Text => "TBD");
+   Draw_Info (Point => (X => 367, Y => 122), Text => "TBD");
+
+   -- Bottom Row
    Draw_Info (Point => (X => 9, Y => 198), Text => "Speed (MPH)");
    Draw_Info (Point => (X => 128, Y => 198), Text => "Charge (%)");
    Draw_Info (Point => (X => 248, Y => 198), Text => "Gear");
    Draw_Info (Point => (X => 367, Y => 198), Text => "Power (kW)");
    GUI.Current_Background_Color := GUI.Default_Background_Color;
-
-   Put (X => 173, Y => 9, Msg => "Make:");
-   Put (X => 228, Y => 9, Msg => "Tesla");
-   Put (X => 173, Y => 19, Msg => "Model:");
-   Put (X => 228, Y => 19, Msg => "Model 3");
-   Put (X => 173, Y => 29, Msg => "VIN:");
-   Put (X => 228, Y => 29, Msg => "XXXXXXXXXXXXXXXXX");
 
    Fill_Rounded_Rectangle
      (Rect   => (Position => (7, 92), Width => 161, Height => 20),
@@ -297,8 +318,8 @@ begin
    GUI.Current_Background_Color := GUI.Default_Background_Color;
    GUI.Current_Text_Color       := GUI.Default_Text_Color;
 
-   GUI.Current_Background_Color :=
-     (Alpha => 255, Red => 26, Green => 36, Blue => 46);
+   --  GUI.Current_Background_Color :=
+   --    (Alpha => 255, Red => 26, Green => 36, Blue => 46);
 
    Receiver.Register_Handler
      (ID => CAN_Standard_ID (16#129#),
@@ -311,28 +332,43 @@ begin
      (ID => CAN_Standard_ID (16#118#),
       CB => CAN_Handler.On_DriverSystemStatus'Access);
 
+   Receiver.Register_Handler
+     (ID => CAN_Standard_ID (16#3F5#), CB => CAN_Handler.On_Lighting'Access);
+
+   Receiver.Register_Handler
+     (ID => CAN_Standard_ID (16#405#), CB => CAN_Handler.On_VIN'Access);
+
    loop
-      --  Put (X => 7, Y => 140, Msg => "Recv: " & Receiver.Get'Image);
       Draw_Info
         (Point => (X => 9, Y => 205), Text => "Speed (MPH)",
          Val   => CAN_Handler.Vehicle_Speed_MPH'Image & " ");
 
-      declare
-         Current_Angle : constant Long_Float :=
-           CAN_Handler.Steering_Angle_Degrees;
-      begin
-         if abs (Current_Angle - Last_Steering_Angle) >= 0.5 then
-            GUI.Images.Draw_Image
-              (X0 => 190, Y0 => 50, Image => GUI.Images.Steering_Wheel,
-               Angle_Degrees => Current_Angle);
+      GUI.Images.Draw_Image
+        (X0            => 230, Y0 => 50, Image => GUI.Images.Steering_Wheel,
+         Angle_Degrees => CAN_Handler.Steering_Angle_Degrees);
 
-            Last_Steering_Angle := Current_Angle;
-         end if;
-      end;
+      if CAN_Handler.Left_Turn_Signal then
+         GUI.Images.Draw_Image
+           (X0            => 190, Y0 => 65, Image => GUI.Images.Arrow,
+            Angle_Degrees => 0.0);
+      end if;
+
+      if CAN_Handler.Right_Turn_Signal then
+         GUI.Images.Draw_Image
+           (X0            => 310, Y0 => 65, Image => GUI.Images.Arrow,
+            Angle_Degrees => 180.0);
+      end if;
 
       Draw_Info
         (Point => (X => 248, Y => 205), Text => "Gear",
          Val   => CAN_Handler.To_String (CAN_Handler.Vehicle_Gear));
+
+      Put (X => 173, Y => 9, Msg => "Make:");
+      Put (X => 228, Y => 9, Msg => "Tesla");
+      Put (X => 173, Y => 19, Msg => "Model:");
+      Put (X => 228, Y => 19, Msg => "Model 3");
+      Put (X => 173, Y => 29, Msg => "VIN:");
+      Put (X => 228, Y => 29, Msg => CAN_Handler.VIN_Number);
 
       Display.Update_Layer (1, True);
 
